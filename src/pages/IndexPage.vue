99144dc37bc40aa35fd4d26bef98b4fc32289d17
ghshin <leteu610@gmail.com>
2022-04-07 17:10:39 +0900

    메인 비주얼 컴포넌트 추가

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,22 +1,23 @@
 <template>
   <q-page class="row justify-evenly">
-    <div class="full-width">
-      <TechStackVue></TechStackVue>
+    <div class="full-width full-height scroll-y">
+      <MainVisualVue />
+      <TechStackVue />
     </div>
   </q-page>
 </template>
 
 <script lang="ts">
-import TechStackVue from 'src/components/TechStack.vue';
-import { defineComponent } from 'vue';
+import MainVisualVue from "src/components/MainVisual.vue";
+import TechStackVue from "src/components/TechStack.vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage',
+  name: "IndexPage",
   components: {
-    TechStackVue
+    MainVisualVue,
+    TechStackVue,
   },
-  setup () {
-
-  }
+  setup() {},
 });
 </script>
