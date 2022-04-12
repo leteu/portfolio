43a7346364f02cc79770b99a581ac81c11d7e5cb
ghshin <leteu610@gmail.com>
2022-04-12 12:50:07 +0900

    타이핑 텍스트 수정

--- src/components/Typing.vue
+++ src/components/Typing.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="typing-text d2coding">
     <p>
-      Hello!!!
+      Hello, I'm leteu.
     </p>
     <p>
-      I'm leteu
+      web <span class="gradient-text">frontend</span> Developer.
     </p>
     <p>
-      Web <span class="gradient-text">frontend</span> Developer
+      Always think about
+    </p>
+    <p>
+      <span class="gradient-text">optimize performance.</span>
     </p>
   </div>
 </template>
@@ -35,28 +38,38 @@ export default defineComponent({
     overflow: hidden
     color: #ededed
     &:nth-child(1)
-      width: 7ch
-      animation: type 1s steps(7, end)
+      width: 13.7ch
+      opacity: 0
+      animation: type 1.37s steps(14, end)
       animation-fill-mode: forwards
     &:nth-child(2) 
-      width: 9ch
+      width: 21.3ch
       opacity: 0
-      animation: type2 1s steps(9, end)
-      animation-delay: 1s
+      animation: type2 2.13s steps(22, end)
+      animation-delay: 1.37s
       animation-fill-mode: forwards
     &:nth-child(3)
-      width: 21ch
+      width: 16.5ch
       opacity: 0
-      animation: type3 2s steps(21, end), blink .5s step-end infinite alternate
-      animation-delay: 2s
+      animation: type2 1.65s steps(17, end), blink .5s step-end infinite alternate
+      animation-delay: 3.5s
+      animation-fill-mode: forwards
+    &:nth-child(4)
+      width: 19.5ch
+      opacity: 0
+      animation: type3 1.95s steps(20, end), blink .5s step-end infinite alternate
+      animation-delay: 5.15s
       animation-fill-mode: forwards
 
 @keyframes type
   0%
     width: 0
+  1%
+    opacity: 1
   99.9%
     border-right: .15em solid
   100%
+    opacity: 1
     border: none
 
 @keyframes type2
