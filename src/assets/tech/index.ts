c36ac1096ec176b60a932556e92de731c3aa2081
ghshin <leteu610@gmail.com>
2022-04-13 17:59:17 +0900

    add tech svg icon file

---
+++ src/assets/tech/index.ts
@@ -0,0 +1,32 @@
+import vuejs from "./vuejs.svg";
+import javascript from "./javascript.svg";
+import typescript from "./typescript.svg";
+import flutter from "./flutter.svg";
+import sass from "./sass.svg";
+import html from "./html.svg";
+import css from "./css.svg";
+import webpack from "./webpack.svg";
+import vite from "./vite.svg";
+import electron from "./electron.svg";
+import git from "./git.svg";
+import gitlab from "./gitlab.svg";
+import slack from "./slack.svg";
+import postcss from "./postcss.svg";
+
+
+export {
+  vuejs,
+  javascript,
+  typescript,
+  flutter,
+  sass,
+  html,
+  css,
+  webpack,
+  vite,
+  electron,
+  git,
+  gitlab,
+  slack,
+  postcss
+}
