9ebde076f0813dcf7b1f6f420ff1943fe07dba86
ghshin <leteu610@gmail.com>
2022-04-14 17:48:08 +0900

    타이핑 시간 축소

--- src/components/Typing.vue
+++ src/components/Typing.vue
@@ -42,25 +42,25 @@ export default defineComponent({
     &:nth-child(1)
       width: 13.7ch
       opacity: 0
-      animation: type 1.37s steps(14, end)
+      animation: type 0.7s steps(14, end)
       animation-fill-mode: forwards
     &:nth-child(2) 
       width: 21.3ch
       opacity: 0
-      animation: type2 2.13s steps(22, end)
-      animation-delay: 1.37s
+      animation: type2 1s steps(22, end)
+      animation-delay: 0.7s
       animation-fill-mode: forwards
     &:nth-child(3)
       width: 16.5ch
       opacity: 0
-      animation: type2 1.65s steps(17, end), blink .5s step-end infinite alternate
-      animation-delay: 3.5s
+      animation: type2 1s steps(17, end), blink .5s step-end infinite alternate
+      animation-delay: 2s
       animation-fill-mode: forwards
     &:nth-child(4)
       width: 19.5ch
       opacity: 0
-      animation: type3 1.95s steps(20, end), blink .5s step-end infinite alternate
-      animation-delay: 5.15s
+      animation: type3 1s steps(20, end), blink .5s step-end infinite alternate
+      animation-delay: 3s
       animation-fill-mode: forwards
 
 @keyframes type
