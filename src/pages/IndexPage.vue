9cdbccec8dd81632c48037252e81c82eb794abad
ghshin <leteu610@gmail.com>
2022-04-15 09:48:57 +0900

    프로젝트 섹션 생성

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,6 +3,7 @@
     <div class="full-width">
       <MainVisualVue />
       <TechStackVue />
+      <ProjectSectionVue />
 
       <div class="q-pa-sm text-center full-width">
         <div class="text-grey-6">
@@ -17,12 +18,14 @@
 import { defineComponent } from "vue";
 import MainVisualVue from "src/components/MainVisual.vue";
 import TechStackVue from "src/components/TechStack.vue";
+import ProjectSectionVue from "src/components/ProjectSection.vue";
 
 export default defineComponent({
   name: "IndexPage",
   components: {
     MainVisualVue,
     TechStackVue,
+    ProjectSectionVue,
   },
   setup() {},
 });
